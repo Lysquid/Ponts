a98e13f7bdc131b4228594a5bf3d91430ff2df53
Lysquid <romainbenoit21560@gmail.com>
2022-04-04 10:24:49 +0200

    debut prix

--- src/ponts/physique/barres/BarreBois.java
+++ src/ponts/physique/barres/BarreBois.java
@@ -11,10 +11,8 @@ public class BarreBois extends Barre {
     public BarreBois(World world, Liaison liaison1, Liaison liaison2) {
         super(world, liaison1, liaison2);
 
-        forceMax = 4000f;
         couleurRemplissage = Color.decode("#ba754a");
-        this.largeur = 1;
-
+        
     }
 
 }
